dafba6a2f5e53bbd8f6df5cfedfcaa38221473e3
asimimamabdullah <asimimamabdullah@gmail.com>
2020-12-20 18:49:01 +0500

    forth/

--- app.js
+++ app.js
@@ -381,7 +381,7 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(process.env.PORT||3000, function() {
+app.listen(process.env.PORT, function() {
   console.log("Server started successfully");
 });
 
